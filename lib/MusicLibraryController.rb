58166c9690f32024248dc893647db51fd69caaf9
Jane Justiz <janejustiz@gmail.com>
2018-08-23 02:46:44 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -75,7 +75,8 @@ class MusicLibraryController
   def play_song
     puts "Which song number would you like to play?"
     input = gets.strip
-    Song.all.sort_by!{|word| word.name}.each_with_index do |song, index|
+    x = Song.all.sort_by!{|word| word.name}
+    x.each_with_index do |song, index|
       if index == input
         puts "Playing #{song.name} by #{song.artist.name}"
       end
