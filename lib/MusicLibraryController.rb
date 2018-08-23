a21b893d4249337a6a1dac333bb48577acf27ebc
Jane Justiz <janejustiz@gmail.com>
2018-08-23 01:15:29 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -39,7 +39,7 @@ class MusicLibraryController
   end
 
   def list_songs
-    Song.all.name.sort!
+    Song.all.sort!
     Song.all.each_with_index do |song, index|
       puts "#{index + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
